67c555d27a2ceabe2f83e121e2b26ee4abe5516d
Obakeng <obakeng@Obakengs-MacBook-Pro.local>
2020-08-20 06:46:21 +0200

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent, pathMatch: 'full'},
+  {path: 'register', component: RegisterComponent, pathMatch: 'full'},
+  {path: 'doctors', component: DoctorsComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DoctorProfileComponent } from './components/doctors/doctor-profile/doctor-profile.component';
+import { DoctorListComponent } from './components/doctors/doctor-list/doctor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    DoctorsComponent,
+    LoginComponent,
+    RegisterComponent,
+    DoctorProfileComponent,
+    DoctorListComponent
   ],
   imports: [
     BrowserModule,
